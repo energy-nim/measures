b91f0e6a878a44b88b7256af1d46667ee5570e53
JeysonFlores <jeyson.flores@hotmail.com>
2023-05-28 15:14:27 -0700

    Added equality operator, renamed methods and formatted file

--- src/units/temperature.nim
+++ src/units/temperature.nim
@@ -7,23 +7,23 @@ type
     Kelvin,
     Farenheit
   Temperature* = ref object
-    val*: float
-    unit*: TemperatureUnit
-  
-  
+    val: float
+    unit: TemperatureUnit
+
+
 proc `°C`*(val: float): Temperature =
   result = Temperature(val: val, unit: Celsius)
 
 proc `°F`*(val: float): Temperature =
   result = Temperature(val: val, unit: Farenheit)
-  
+
 proc `°K`*(val: float): Temperature =
   result = Temperature(val: val, unit: Kelvin)
 
 
 const equalityTable = {
   (Celsius, Celsius): proc(c: float): float = c * 1,
-  (Celsius, Farenheit):  proc(c: float): float = (c * 1.8) + 32,
+  (Celsius, Farenheit): proc(c: float): float = (c * 1.8) + 32,
   (Celsius, Kelvin): proc(c: float): float = c + 273.15,
   (Farenheit, Farenheit): proc(f: float): float = f * 1,
   (Farenheit, Celsius): proc(f: float): float = (f - 32) / 1.8,
@@ -34,15 +34,24 @@ const equalityTable = {
 }.toTable
 
 
-proc isOf*(self: Temperature, unit: TemperatureUnit): bool =
-  result = self.unit == unit 
+proc isOf*(self: Temperature, unit: TemperatureUnit): bool {.inline.} = self.unit == unit
 
-proc getAs*(self: Temperature, unit: TemperatureUnit): float =
+proc getValue*(self: Temperature): float {.inline.} = self.val
+
+proc getUnit*(self: Temperature): TemperatureUnit {.inline.} = self.unit
+
+proc getValueAs*(self: Temperature, unit: TemperatureUnit): float =
   result = equalityTable[(self.unit, unit)](self.val)
 
 proc to*(self: Temperature, unit: TemperatureUnit) =
   if self.unit == unit:
     return
-  
-  self.val = self.getAs(unit)
+
+  self.val = self.getValueAs(unit)
   self.unit = unit
+
+proc `==`*(self, other: Temperature): bool =
+  if self.unit == other.unit:
+    result = self.val == other.val
+  else:
+    result = self.val == other.getValueAs(self.unit)
--- tests/test_temperatures.nim
+++ tests/test_temperatures.nim
@@ -6,41 +6,48 @@ import units/temperature
 test "celsius tests":
   let testingTemp = 25.°C
   check testingTemp.isOf(Celsius)
-  check testingTemp.getAs(Farenheit) == 77.0
-  check testingTemp.getAs(Kelvin) == 298.15
+  check testingTemp.getValueAs(Farenheit) == 77.0
+  check testingTemp.getValueAs(Kelvin) == 298.15
   
   testingTemp.to(Farenheit)
   check testingTemp.isOf(Farenheit)
-  check testingTemp.getAs(Celsius) == 25.0
+  check testingTemp.getValueAs(Celsius) == 25.0
 
   testingTemp.to(Kelvin)
   check testingTemp.isOf(Kelvin)
-  check testingTemp.getAs(Celsius) == 25.0
+  check testingTemp.getValueAs(Celsius) == 25.0
+
+  check 25.°C == 25.°C
+  check 25.°C == 77.°F
+  check 25.°C == 298.15.°K
+  check 25.°C != 25.1.°C
+  check 25.°C != 0.°F
+  check 25.°C != 0.°K
 
 test "farenheit tests":
   let testingTemp = 77.°F
   check testingTemp.isOf(Farenheit)
-  check testingTemp.getAs(Celsius) == 25.0
-  check testingTemp.getAs(Kelvin) == 298.15
+  check testingTemp.getValueAs(Celsius) == 25.0
+  check testingTemp.getValueAs(Kelvin) == 298.15
 
   testingTemp.to(Celsius)
   check testingTemp.isOf(Celsius)
-  check testingTemp.getAs(Farenheit) == 77.0
+  check testingTemp.getValueAs(Farenheit) == 77.0
   
   testingTemp.to(Kelvin)
   check testingTemp.isOf(Kelvin)
-  check testingTemp.getAs(Farenheit) == 77.0
+  check testingTemp.getValueAs(Farenheit) == 77.0
 
 test "kelvin tests":
   let testingTemp = 298.15.°K
   check testingTemp.isOf(Kelvin)
-  check testingTemp.getAs(Celsius) == 25.0
-  check testingTemp.getAs(Farenheit) == 77.0
+  check testingTemp.getValueAs(Celsius) == 25.0
+  check testingTemp.getValueAs(Farenheit) == 77.0
 
   testingTemp.to(Celsius)
   check testingTemp.isOf(Celsius)
-  check testingTemp.getAs(Kelvin) == 298.15
+  check testingTemp.getValueAs(Kelvin) == 298.15
 
   testingTemp.to(Farenheit)
   check testingTemp.isOf(Farenheit)
-  check testingTemp.getAs(Kelvin) == 298.15
+  check testingTemp.getValueAs(Kelvin) == 298.15
